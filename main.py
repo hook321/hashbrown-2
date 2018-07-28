10f0fd1d41433d4b8495df2e70a9b2132c74a7e1
Devin Keeney <bkeeneykid@me.com>
2018-07-28 01:22:42 -0500

    Make the hash message clearer

--- main.py
+++ main.py
@@ -58,8 +58,9 @@ async def hash(ctx):
     hash_str = ''.join(random.choices(string.ascii_uppercase + string.digits, k=8))
     hash_obj = Hash(hash=hash_str)
     session.add(hash_obj)
-    await ctx.send("Your hash is `{}`. Keep this secret, and copy it to your survey. This message will be deleted in 60 seconds so that we cannot track your hash, so please save your hash now. ".format(hash_str),
-                   delete_after=60)
+    await ctx.send("Your hash is `{}`. Keep this secret, and copy it to your survey. This message will be deleted in 60 \
+    seconds so that we cannot track your hash, and you won't be able to request it again, so please save your hash now. "
+                   .format(hash_str),delete_after=60)
     user_check = UserCheck(user_id=ctx.author.id)
     user_check.time_hashed = datetime.datetime.utcnow()
     session.add(user_check)
